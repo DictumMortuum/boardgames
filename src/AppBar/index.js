cadcce60276c1f8f687c068d4a755ab5263b6a1d
DictumMortuum <theio.vrefos@gmail.com>
2023-08-05 17:22:34 +0300

    fixed appbar on mobile view

--- src/AppBar/index.js
+++ src/AppBar/index.js
@@ -2,28 +2,17 @@ import React from 'react';
 import { YearContext } from '../context';
 import { Button, Toolbar, Checkbox } from '@mui/material';
 import { ArrowRight, ArrowLeft } from '@mui/icons-material';
-import { useTheme } from '@mui/material/styles';
-import useMediaQuery from '@mui/material/useMediaQuery';
 import HomeIcon from '@mui/icons-material/Home';
 import IconButton from '@mui/material/IconButton';
 import { Link } from 'react-router-dom';
 
 const Component = () => {
-  const theme = useTheme();
-  const matches = useMediaQuery(theme.breakpoints.up('sm'));
   const { year, setYear, yearFlag, setYearFlag } = React.useContext(YearContext);
   const previous_year = year-1;
   const next_year = year+1;
 
-  let justifyContent;
-  if (matches) {
-    justifyContent = "space-between";
-  } else {
-    justifyContent = "center";
-  }
-
   return (
-    <Toolbar sx={{ color: "white", justifyContent }}>
+    <Toolbar sx={{ color: "white", justifyContent: "space-between" }}>
       <Link to="/">
         <IconButton sx={{ color: "white" }}>
           <HomeIcon />
